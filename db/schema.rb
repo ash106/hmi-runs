77ed4cf3db96990118210285a843feb60fffac98
Alex Howington <alex.howington@gmail.com>
2014-10-08 23:33:22 -0500

    Migrate that sucker

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140902190058) do
+ActiveRecord::Schema.define(version: 20141009043233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,5 +45,15 @@ ActiveRecord::Schema.define(version: 20140902190058) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "workouts", force: true do |t|
+    t.date     "date_of"
+    t.integer  "length"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "workouts", ["user_id"], name: "index_workouts_on_user_id", using: :btree
 
 end
