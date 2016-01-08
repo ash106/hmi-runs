4929371557625b6738e4f2894ee6e238a74c18b0
Alex Howington <alex.howington@gmail.com>
2016-01-08 03:26:15 -0600

    Reformat dates on runs index page

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -1,1 +1,3 @@
 Date::DATE_FORMATS[:shorthand] = "%m/%d/%Y"
+Date::DATE_FORMATS[:short_year] = "%m/%d/%y"
+Date::DATE_FORMATS[:month_names] = lambda { |date| date.strftime("%b #{date.day.ordinalize}, %Y") }
